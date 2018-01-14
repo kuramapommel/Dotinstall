2fbcef8b7f66eb4d310c1e683441e4ecae8e6c89
Pommel Kurama <pommel.pg@gmail.com>
2018-01-14 18:49:35 +0900

    #23

---
+++ Unity/projects/BallGame/Assets/EnemiesScript.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemiesScript : MonoBehaviour
+{
+  public Transform enemy;
+
+  // Update is called once per frame
+  void Update()
+  {
+    if (Time.frameCount % 60 == 0)
+    {
+      Instantiate(enemy, new Vector3(Random.Range(-5.0F, 5.0F), 1, 8), transform.rotation);
+    }
+
+  }
+}
---
+++ Unity/projects/BallGame/Assets/EnemyScript.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyScript : MonoBehaviour
+{
+
+  // Update is called once per frame
+  void Update()
+  {
+    var pos = transform.position;
+    pos.z -= 0.1F;
+    transform.position = pos;
+
+    transform.Rotate(1, 1, 1);
+  }
+}
