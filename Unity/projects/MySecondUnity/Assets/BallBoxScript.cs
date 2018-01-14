7e73ed9fca0492018a50b032341119b6815f83ba
Pommel Kurama <pommel.pg@gmail.com>
2018-01-14 17:06:32 +0900

    #17

---
+++ Unity/projects/MySecondUnity/Assets/BallBoxScript.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallBoxScript : MonoBehaviour
+{
+
+  public Transform ball;
+
+  // Use this for initialization
+  void Start()
+  {
+
+  }
+
+  // Update is called once per frame
+  void Update()
+  {
+    if (Input.GetButtonUp("Jump"))
+    {
+      Instantiate(ball, transform.position, transform.rotation);
+    }
+  }
+}
