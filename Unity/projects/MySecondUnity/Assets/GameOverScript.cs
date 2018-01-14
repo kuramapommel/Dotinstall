55f9a93933c09d458a2e572fd6fd34dca5898f42
Pommel Kurama <pommel.pg@gmail.com>
2018-01-14 17:14:03 +0900

    #18

---
+++ Unity/projects/MySecondUnity/Assets/GameOverScript.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameOverScript : MonoBehaviour
+{
+  public GUIStyle style;
+
+  // Use this for initialization
+  void Start()
+  {
+
+  }
+
+  // Update is called once per frame
+  void Update()
+  {
+
+  }
+
+  void OnGUI()
+  {
+    GUI.Label(new Rect(10, 10, 200, 50), "Game Over...", style);
+  }
+}
