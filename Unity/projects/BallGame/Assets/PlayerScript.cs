8029a196f66d42b48a970c915cf6de6aaee7ad0a
Pommel Kurama <pommel.pg@gmail.com>
2018-01-14 19:03:16 +0900

    #25

--- Unity/projects/BallGame/Assets/PlayerScript.cs
+++ Unity/projects/BallGame/Assets/PlayerScript.cs
@@ -11,8 +11,20 @@ public class PlayerScript : MonoBehaviour
     transform.Translate(x * 0.2F, 0, 0);
   }
 
-  void OnCollisionEnter()
+  void OnCollisionEnter(Collision collision)
   {
+    if (collision.gameObject.name == "Enemy(Clone)")
+    {
+      var localScale = transform.localScale;
+      localScale.x -= Random.Range(0.1F, 0.5F);
+
+      if (localScale.x < 1.0F)
+      {
+        localScale.x = 1.0F;
+      }
+
+      transform.localScale = localScale;
+    }
 
   }
 }
